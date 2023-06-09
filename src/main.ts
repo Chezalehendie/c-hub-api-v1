fdc27135756ba545c0d4f701471748b1dc786591
Hendrina Chezale <bsc-com-24-19@unima.ac.mw>
2023-06-09 23:53:13 +0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     .setTitle('chanco online community')
     .setDescription('The chanco online community API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('entertainment,business and health')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
